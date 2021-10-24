95bf6077597f5baf6c337fe9ab6fe5954e4f33dc
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-10-24 22:06:06 +0300

    Created Splash and Starter Activity and inserted blue bird icon (with diff screen formats) 🎨

---
+++ Android/app/src/main/java/com/cemaltuysuz/twitter/StarterActivity.kt
@@ -0,0 +1,16 @@
+package com.cemaltuysuz.twitter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class StarterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starter)
+
+    }
+}
