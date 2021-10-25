a7b0226928e547463afc645dc9252711db53d3a1
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-10-25 22:01:09 +0300

    Revised theme and created new toolbar for starter activity | Util.kt inserted new function for user is online or offline

--- Android/app/src/main/java/com/cemaltuysuz/twitter/SplashActivity.kt
+++ Android/app/src/main/java/com/cemaltuysuz/twitter/SplashActivity.kt
@@ -1,13 +1,45 @@
+/*
+ * *
+ *  * Created by cemaltuysuz on 10/25/21 9:35 PM
+ *  * Copyright (c) 2021 . All rights reserved.
+ *  * Last modified 10/25/21 9:32 PM
+ *
+ */
+
 package com.cemaltuysuz.twitter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.cemaltuysuz.twitter.utils.isOnline
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * With this activity i decide which screen the user should go to
+ * */
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var routerIntent: Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        supportActionBar?.hide()
+
+        /**
+         * the user is shown the logo for at least 2 second + The process of learning the online status
+         * */
+
+        lifecycleScope.launch {
+            delay(2300)
+            routerIntent = if (isOnline()){
+                Intent(applicationContext,MainActivity::class.java)
+            }else{
+                Intent(applicationContext,StarterActivity::class.java)
+            }
+            startActivity(routerIntent)
+            finish()
+        }
 
     }
 }
--- Android/app/src/main/java/com/cemaltuysuz/twitter/utils/Util.kt
+++ Android/app/src/main/java/com/cemaltuysuz/twitter/utils/Util.kt
@@ -1,2 +1,8 @@
 package com.cemaltuysuz.twitter.utils
 
+import com.google.firebase.auth.FirebaseAuth
+
+/**
+ * Learn the current user online or offline
+ * */
+fun isOnline() = FirebaseAuth.getInstance().currentUser != null
