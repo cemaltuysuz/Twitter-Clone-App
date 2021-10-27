fed3f213dc14bacfdf5cf50e1b216e3d92e82693
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-10-27 19:15:35 +0300

    inserted Strings for Welcome Fragment

---
+++ Android/app/src/main/java/com/cemaltuysuz/twitter/ui/fragments/starter/WelcomeFragment.kt
@@ -0,0 +1,26 @@
+/*
+ * *
+ *  * Created by cemaltuysuz on 10/27/21 7:01 PM
+ *  * Copyright (c) 2021 . All rights reserved.
+ *  * Last modified 10/27/21 7:01 PM
+ *
+ */
+
+package com.cemaltuysuz.twitter.ui.fragments.starter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cemaltuysuz.twitter.R
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
