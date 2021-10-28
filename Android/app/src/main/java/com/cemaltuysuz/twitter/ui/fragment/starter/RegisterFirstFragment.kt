b06c0e1986796a457b4436712bad75afa404a277
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-10-28 23:08:53 +0300

    Created register fragment 👻 | inserted some views -> Step 1

---
+++ Android/app/src/main/java/com/cemaltuysuz/twitter/ui/fragment/starter/RegisterFirstFragment.kt
@@ -0,0 +1,28 @@
+/*
+ * *
+ *  * Created by cemaltuysuz on 10/28/21 10:35 PM
+ *  * Copyright (c) 2021 . All rights reserved.
+ *  * Last modified 10/28/21 10:35 PM
+ *
+ */
+
+/**
+ * With this class, I get the necessary information from the user.
+ * In this class the register steps in first
+ * */
+package com.cemaltuysuz.twitter.ui.fragment.starter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cemaltuysuz.twitter.R
+
+class RegisterFirstFragment : Fragment(R.layout.fragment_register_first) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
