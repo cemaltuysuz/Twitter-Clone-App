4343332cb7b0fcb59ff1a08d6e8f7eca7f3c03af
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-11-05 19:24:21 +0300

    Renamed some drawable files 🚀 | setup action bar with navcontroller in starter activity 🚀 | created bottom help bar in first register fragment

--- Android/app/src/main/java/com/cemaltuysuz/twitter/StarterActivity.kt
+++ Android/app/src/main/java/com/cemaltuysuz/twitter/StarterActivity.kt
@@ -2,15 +2,23 @@ package com.cemaltuysuz.twitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class StarterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starter)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.starter_navigation_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+                .setupWithNavController(navController, appBarConfiguration)
 
     }
 }
--- Android/app/src/main/java/com/cemaltuysuz/twitter/ui/fragment/starter/WelcomeFragment.kt
+++ Android/app/src/main/java/com/cemaltuysuz/twitter/ui/fragment/starter/WelcomeFragment.kt
@@ -11,14 +11,33 @@ package com.cemaltuysuz.twitter.ui.fragment.starter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.Navigation
 import com.cemaltuysuz.twitter.R
+import com.cemaltuysuz.twitter.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+    private var fragmentWelcomeBinding:FragmentWelcomeBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fragmentWelcomeBinding = FragmentWelcomeBinding.bind(view)
+        val binding = fragmentWelcomeBinding!!
 
+
+        binding.routeCreateAccount.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToRegisterFirstFragment2()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+
+
+    }
+
+    override fun onDestroy() {
+        fragmentWelcomeBinding = null
+        super.onDestroy()
     }
 
 }
